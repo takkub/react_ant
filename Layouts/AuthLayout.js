165fbf7d67199d4028fb7944c5a86bd992f8a233
NiranDx <niran3688@gmail.com>
2025-05-19 15:41:12 +0700

    add change theme

--- Layouts/AuthLayout.js
+++ Layouts/AuthLayout.js
@@ -3,10 +3,12 @@ import { SessionProvider } from 'next-auth/react';
 import {ConfigProvider} from 'antd';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import React from "react";
-import { lightTheme, WSOLTheme } from "../theme/theme";
+import { WSOLTheme, darkTheme } from "../theme/theme";
+import { useTheme } from '@/store/context/ThemeContext';
 export default function LoginLayout({ children }) {
+    const { theme } = useTheme();
     return (
-        <ConfigProvider theme={WSOLTheme}>
+        <ConfigProvider theme={theme === "light" ? WSOLTheme : darkTheme}>
             <AntdRegistry>
                 <SessionProvider>
                     {children}
---
+++ store/context/ThemeContext.js
@@ -0,0 +1,31 @@
+"use client"
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light'); 
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'light' || storedTheme === 'dark') {
+      setTheme(storedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
